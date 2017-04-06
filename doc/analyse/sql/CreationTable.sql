dcdf23e414f325232ee030076877776bc1c8a062
leo <pinot.leo@gmail.com>
2017-04-06 13:20:56 +0200

    modiication sql

--- doc/analyse/sql/CreationTable.sql
+++ doc/analyse/sql/CreationTable.sql
@@ -1,17 +1,17 @@
 create table TYPE_UTILISATEUR(
-	id_typeutil_ SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+	id_typeutil SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
 	libelle_typeutil VARCHAR(20) NOT NULL,
-	PRIMARY KEY (id_typeutil_)
+	PRIMARY KEY (id_typeutil)
 );
 
 create table UTILISATEUR (
-	_id_utilisateur_ SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,  
+	id_utilisateur SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,  
 	nom_utilisateur VARCHAR(20) NOT NULL, 
 	prenom_utilisateur VARCHAR(20),
 	login_utilisateur VARCHAR(20) NOT NULL, 
 	password_utilisateur VARCHAR(20) NOT NULL, 
-	id_typeutil SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
-	PRIMARY KEY (_id_utilisateur_),
+	id_typeutil SMALLINT UNSIGNED, 
+	PRIMARY KEY (id_utilisateur),
 	FOREIGN KEY (id_typeutil) REFERENCES TYPE_UTILISATEUR(id_typeutil)
 	
 	
@@ -19,72 +19,89 @@ create table UTILISATEUR (
 
 
 create table SONDE (
-	_id_sonde_ SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	id_sonde SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
 	nom_sonde VARCHAR(20) NOT NULL, 
 	prenom_sonde VARCHAR(20) NOT NULL, 
 	sexe_sonde VARCHAR(20) NOT NULL, 
 	datenaiss_sonde DATE, 
 	tel_sonde VARCHAR(14)
-	PRIMARY KEY (_id_sonde_)
+	id_csp SMALLINT UNSIGNED,
+	id_adresse SMALLINT UNSIGNED,
+	PRIMARY KEY (id_sonde),
+	FOREIGN KEY (id_csp) REFERENCES CSP(id_csp),
+	FOREIGN KEY (id_adresse) REFERENCES ADRESSE(id_adresse)
 );
 
 create table QUESTIONNAIRE (
-	_id_questionnaire_ SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
-	intitule_questionnaire VARCHAR(100), 
+	id_questionnaire SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+	intitule_questionnaire VARCHAR(100) NOT NULL, 
 	dateheurelim_questionnaire DATE, 
 	accessible_questionnaire BOOLEAN, 
 	termine_questionnaire BOOLEAN, 
 	analyse_questionnaire BOOLEAN,
-	#aaaa-mm-jj_hh:mm:ss:ms, 
+	dateheure_creation DATE, 
 	url_analyse VARCHAR(100),  
-	id_utilisateur SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
-	id_panel SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
-	id_entreprise SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	id_utilisateur SMALLINT UNSIGNED, 
+	id_panel SMALLINT UNSIGNED, 
+	id_entreprise SMALLINT UNSIGNED,
 	FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEUR(id_utilisateur),
 	FOREIGN KEY (id_panel) REFERENCES PANEL(id_panel),
 	FOREIGN KEY (id_entreprise) REFERENCES ENREPRISE(id_entreprise),
-	PRIMARY KEY (_id_questionnaire_)
+	PRIMARY KEY (id_questionnaire)
 );
 
-create table SONDER (
-	_id_utilisateur_ SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
-	_id_sonde_ SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
-	_id_questionnaire_ SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-	 dateheure_sondage DATE,
-	PRIMARY KEY (_id_utilisateur_),
-	PRIMARY KEY (_id_sonde_),
-	PRIMARY KEY (_id_questionnaire_),
-	FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEUR(id_utilisateur),
-	FOREIGN KEY (_id_sonde_) REFERENCES SONDE(_id_sonde_),
-	FOREIGN KEY (id_entreprise) REFERENCES ENREPRISE(id_entreprise)
+create table REMPLIR(
+	id_sonde SMALLINT UNSIGNED, 
+	id_questionnaire SMALLINT UNSIGNED,
+	id_utilisateur SMALLINT UNSIGNED,
+	date_remplissage DATE NOT NULL,
+	PRIMARY KEY (id_sonde),
+	PRIMARY KEY (id_questionnaire),
+	FOREIGN KEY (id_sonde) REFERENCES SONDE(id_sonde),
+	FOREIGN KEY (id_questionnaire) REFERENCES QUESTIONNAIRE(id_questionnaire),
+	FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEUR(id_utilisateur)
 
 );
+
+create table ANALYSER(
+	id_questionnaire SMALLINT UNSIGNED,
+	id_utilisateur SMALLINT UNSIGNED,
+	date_analyse DATE NOT NULL,
+	url_analyse VARCHAR(100),
+	PRIMARY KEY (id_utilisateur),
+	PRIMARY KEY (id_questionnaire),
+	PRIMARY KEY (date_analyse),
+	FOREIGN KEY (id_questionnaire) REFERENCES QUESTIONNAIRE(id_questionnaire),
+	FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEUR(id_utilisateur)
+
+);
+
 
 create table VILLE (
-	_codeinsee_ville_ VARCHAR(5) NOT NULL, 
+	codeinsee_ville VARCHAR(5) NOT NULL, 
 	nom_ville VARCHAR(50), 
 	codepostal_ville VARCHAR(5),
-	PRIMARY KEY(_codeinsee_ville_)
+	PRIMARY KEY(codeinsee_ville)
 );
 
 create table CSP (
-	_id_csp_ SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+	id_csp SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
 	libelle_csp VARCHAR(100),
-	PRIMARY KEY(_id_csp_)
+	PRIMARY KEY(id_csp)
 );
 
 
 create table APPARTENIR (
-	_id_panel_ SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
-	_id_sonde SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-	PRIMARY KEY (_id_panel_),
-	PRIMARY KEY (_id_sonde),
-	FOREIGN KEY (_id_panel_) REFERENCES PANEL(_id_panel_),
-	FOREIGN KEY (_id_sonde_) REFERENCES SONDE(_id_sonde_),
+	id_panel SMALLINT UNSIGNED , 
+	id_sonde SMALLINT UNSIGNED,
+	PRIMARY KEY (id_panel),
+	PRIMARY KEY (id_sonde),
+	FOREIGN KEY (id_panel) REFERENCES PANEL(_id_panel_),
+	FOREIGN KEY (id_sonde) REFERENCES SONDE(_id_sonde_)
 );
 
 create table ADRESSE (
-	_id_adresse_ SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+	id_adresse SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
 	numero_adresse INTEGER(10), 
 	bister_adresse VARCHAR(3), 
 	libellevoie_adresse VARCHAR(150), 
@@ -92,33 +109,33 @@ create table ADRESSE (
 	etage_adresse VARCHAR(3), 
 	escalier_adresse VARCHAR(2), 
 	appartement_adresse VARCHAR(10), 
-	codeinsee_ville VARCHAR(5) NOT NULL,
-	PRIMARY KEY (_id_adresse_),
+	codeinsee_ville VARCHAR(5),
+	PRIMARY KEY (id_adresse),
 	FOREIGN KEY (codeinsee_ville) REFERENCES VILLE(codeinsee_ville)
 
 );
 
 create table CHOIX_MULTIPLE (
-	_id_choixmul_ SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+	id_choixmul SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
 	intitule_choixmul VARCHAR(100), 
-	id_question SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT
-	PRIMARY KEY (_id_choixmul_),
+	id_question SMALLINT UNSIGNED,
+	PRIMARY KEY (id_choixmul),
 	FOREIGN KEY (id_question) REFERENCES QUESTION(id_question)
 );
 
 create table PANEL (
-	_id_panel_ SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+	id_panel SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
 	intitule_panel VARCHAR(100),
 	PRIMARY KEY (intitule_panel)
 );
 
 create table QUESTION (
-	_id_question_ SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+	id_question SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
 	enonce_question VARCHAR(100), 
 	urlanal_question VARCHAR(100), 
-	#id_typequest SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
-	#id_questionnaire SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-	PRIMARY KEY (_id_question_),
+	id_typequest SMALLINT UNSIGNED, 
+	id_questionnaire SMALLINT UNSIGNED,
+	PRIMARY KEY (id_question),
 	FOREIGN KEY (id_typequest) REFERENCES TYPE_QUESTION(id_typequest),
 	FOREIGN KEY (id_questionnaire) REFERENCES QUESTIONNAIRE(id_questionnaire)
 );
@@ -126,54 +143,79 @@ create table QUESTION (
 
 
 create table ENTREPRISE (
-	_id_entreprise_ SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
-	nom_entreprise VARCHAR(100), 
+	id_entreprise SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+	nom_entreprise VARCHAR(100) NOT NULL, 
 	tel_entreprise VARCHAR(14), 
 	email_entreprise VARCHAR(133), 
-	id_adresse SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-	PRIMARY KEY (_id_entreprise_),
+	id_adresse SMALLINT UNSIGNED,
+	PRIMARY KEY (id_entreprise),
 	FOREIGN KEY (id_adresse) REFERENCES ADRESSE(id_adresse)
 );
 
 create table TYPE_QUESTION (
-	_id_typequest_ SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+	id_typequest SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
 	libelle_typequest VARCHAR(10),
-	PRIMARY KEY (_id_typequest_)
+	PRIMARY KEY (id_typequest)
 );
 
 create table ENREGISTRER_NOTE (
-	_#id_question_, 
-	_#aaaa-mm-jj_hh:mm:ss:ms_, 
-	note
+	id_question SMALLINT UNSIGNED, 
+	dateheure_note DATE NOT NULL, 
+	note VARCHAR (5),
+	PRIMARY KEY(id_question),
+	PRIMARY KEY(dateheure_note),
+	FOREIGN KEY(id_question) REFERENCES QUESTION(id_question)
 )
 
 create table ENREGISTRER_REPONSE (
-	_#id_question_, 
-	_#aaaa-mm-jj_hh:mm:ss:ms_, 
-	commentaire
+	id_question SMALLINT UNSIGNED, 
+	dateheure_reponse DATE NOT NULL, 
+	commentaire VARCHAR (140),
+	PRIMARY KEY(id_question),
+	PRIMARY KEY(dateheure_reponse),
+	FOREIGN KEY(id_question) REFERENCES QUESTION(id_question)
 )
 
 
 create table ENREGISTRER_CHOIX (
-	_#id_question_, 
-	_#aaaa-mm-jj_hh:mm:ss:ms_,
-	_#id_choixmul_
+	id_question SMALLINT UNSIGNED, 
+	dateheure_choix DATE NOT NULL, 
+	id_choixmul SMALLINT UNSIGNED,
+	PRIMARY KEY(id_question),
+	PRIMARY KEY(dateheure_choix),
+	FOREIGN KEY(id_question) REFERENCES QUESTION(id_question),
+	FOREIGN KEY(id_choixmul) REFERENCES CHOIX_MULTIPLE(id_choixmul)
 )
 
 
 create table ENREGISTRER_CLASSEMENT (
-	_#id_question_,
-	 _#aaaa-mm-jj_hh:mm:ss:ms_, 
-	_#id_choixmul_, 
-	position
+	id_question SMALLINT UNSIGNED, 
+	dateheure_classement DATE NOT NULL, 
+	id_choixmul SMALLINT UNSIGNED,
+	position VARCHAR(2),
+	PRIMARY KEY(id_question),
+	PRIMARY KEY(dateheure_classement),
+	PRIMARY KEY(id_choixmul),
+	FOREIGN KEY(id_question) REFERENCES QUESTION(id_question),
+	FOREIGN KEY(id_choixmul) REFERENCES CHOIX_MULTIPLE(id_choixmul)
+);
+
+create table ENREGISTRER_SELECTION(
+	id_question SMALLINT UNSIGNED, 
+	dateheure_selection DATE NOT NULL, 
+	id_choixmul SMALLINT UNSIGNED,
+	PRIMARY KEY(id_question),
+	PRIMARY KEY(dateheure_selection),
+	PRIMARY KEY(id_choixmul),
+	FOREIGN KEY(id_question) REFERENCES QUESTION(id_question),
+	FOREIGN KEY(id_choixmul) REFERENCES CHOIX_MULTIPLE(id_choixmul)
+
 );
 
 
 
 
 
-
-ENREGISTRER_SELECTION (_#id_question_, _#aaaa-mm-jj_hh:mm:ss'ms_, _#id_choixmul₎
 
 
 
