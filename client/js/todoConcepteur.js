2ecaf2da6421b4e33bb235b8433b5e54233394e4
besancon <julien.besancon@etu.univ-orleans.fr>
2017-05-31 08:33:07 +0200

    test jquery

--- client/js/todoConcepteur.js
+++ client/js/todoConcepteur.js
@@ -1,11 +1,9 @@
 function test(){
 
-	$('#test').append('coucou  ');
+	// $('#test').append('coucou  ');
 
-	$('<div id="div1" class="col-md-12"/>').appendTo('body');
 
-	$('<div id="div2" class="col-md-12" />').appendTo('body');
+	$('#main').append('<div id="div2" class="col-md-12" />');
 
-	$('#div1').append('test div 1');
 	$('#div2').append('test div 2');
 }
