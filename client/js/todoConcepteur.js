459e23e45d51f145aa9f265475135482753d17e2
besancon <julien.besancon@etu.univ-orleans.fr>
2017-05-31 08:25:10 +0200

    petite modif

--- client/js/todoConcepteur.js
+++ client/js/todoConcepteur.js
@@ -1,4 +1,11 @@
 function test(){
 
-	$('#test').append('coucou')
+	$('#test').append('coucou  ');
+
+	$('<div id="div1" class="col-md-12"/>').appendTo('body');
+
+	$('<div id="div2" class="col-md-12" />').appendTo('body');
+
+	$('#div1').append('test div 1');
+	$('#div2').append('test div 2');
 }
