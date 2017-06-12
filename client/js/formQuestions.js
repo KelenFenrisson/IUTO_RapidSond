8fe0f6a51cd299cf67764d04b317b9ccd83d5f0d
Olivier Perichon <perichon@info12-09.iut45.univ-orleans.fr>
2017-06-12 15:11:43 +0200

    ajout formQuestions.js (oubli commit précédent)

---
+++ client/js/formQuestions.js
@@ -0,0 +1,7 @@
+function ajoutReponse(){
+  $('#checkboxReponses').append('<div class="row" id="row">\n<input type="checkbox" id="checkboxreponse" value="reponse"/>\n<input type="text" id="reponse" placeholder="Entrer une réponse"/>\n</div>');
+}
+
+function supprReponse(){
+  $('#row').last().remove();
+}
