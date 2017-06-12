56e1ec8bc2ac4b3f07a1ac02128e08a3adefe8bb
leo <pinot.leo@gmail.com>
2017-06-12 17:17:49 +0200

    merge romeo v2

--- client/js/todoConcepteur.js
+++ client/js/todoConcepteur.js
@@ -30,11 +30,7 @@ function accueilConcepteur(){
 
 		$("#main").empty();
 		$("#main").append('<input type="button" value="Créer un nouveau sondage" class="btn btn-primary btn-lg active top-10 col-md-offset-3 col-md-6 bot-10" onclick="creerFormulaire()">');
-<<<<<<< HEAD
     $("#main").append($(formulaire_Recherche_Sondage).html());
-=======
-		$("#main").append($("<div>").load("formulaire_Recherche_Sondage.html"));
->>>>>>> rom/master
 		// CECI EST POUR LEXEMPLE, à EDITER PAR LA SUITE AVEC UNE RECHERCHE DANS LA BASE
 		for(var i =0;i<4;i++)
 		$("#main").append($(formulaire_Info_Sondage).html());
