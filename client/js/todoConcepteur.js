226d45a18f291a419bd529c923213f00705b7d2d
Olivier Perichon <perichon@info12-09.iut45.univ-orleans.fr>
2017-06-13 08:51:22 +0200

    modifs todoConcepteur.js

--- client/js/todoConcepteur.js
+++ client/js/todoConcepteur.js
@@ -1,12 +1,10 @@
 function accueilConcepteur(){
-
 		$("#main").empty();
 		$("#main").append('<input type="button" value="Créer un nouveau sondage" class="btn btn-primary btn-lg active top-10 col-md-offset-3 col-md-6 bot-10" onclick="creerFormulaire()">');
 		$("#main").append($("<div>").load("formulaire_Recherche_Sondage.html"));
 		// CECI EST POUR LEXEMPLE, à EDITER PAR LA SUITE AVEC UNE RECHERCHE DANS LA BASE
 		for(var i =0;i<4;i++)
 		$("#main").append($("<div>").load("formulaire_Info_Sondage.html"));
-
 }
 
 
